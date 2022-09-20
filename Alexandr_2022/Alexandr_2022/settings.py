9bc3dc2a16f06dd227c463b8b0103556608554cf
2pokek <alkuzmich6479@gmail.com>
2022-09-20 22:10:38 +0300

    why

--- Alexandr_2022/Alexandr_2022/settings.py
+++ Alexandr_2022/Alexandr_2022/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Alexandr_2022.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['Alexandr_2022/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
